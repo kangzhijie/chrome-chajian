60c2ddb3e200541f2b5347b43b79f485a21324c4
kangzhijie <kangzhijie@aiyunxiao.com>
2019-07-08 20:09:05 +0800

    add v-for show the button

--- src/js/app.js
+++ src/js/app.js
@@ -3,11 +3,12 @@
 new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        buttonList: {freeLogin: "免密登录", onlyLogin: "账号密码"}
     },
     methods: {
-        show: function () {
-            console.log("that not good idea")
+        show: function (type) {
+            console.log("that not good idea::", type)
         }
     }
 })
