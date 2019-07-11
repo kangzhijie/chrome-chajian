71a4ca0c247d2c4396b36b6f87f66a4a876df03a
kangzhijie <kangzhijie@aiyunxiao.com>
2019-07-11 18:32:27 +0800

    添加跳转备注 a && window.location.href 的方式

--- src/js/app.js
+++ src/js/app.js
@@ -13,6 +13,8 @@ new Vue({
         show: function (type) {
             console.log("that not good idea::", type);
             window.location.href = ("./template/freelogin.html");
+
+            // 跳转使用 a标签或者 window.location.href ---- 不过还没有实验通过
         }
     }
 })
